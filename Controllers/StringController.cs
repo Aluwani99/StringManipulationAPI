93cdadeac6cca1903242f825ae341713179680ea
Aluwani Rambau <MahlatseR@nhbrc.org.za>
2024-07-18 22:46:57 +0200

    Initial commit

---
+++ Controllers/StringController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Application.Services;
+
+namespace API_Application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StringController : ControllerBase
+    {
+        private readonly StringService _stringService;
+
+        public StringController(StringService stringService)
+        {
+            _stringService = stringService;
+        }
+
+        [HttpGet("reverse-and-check-palindrome")]
+        public IActionResult ReverseAndCheckPalindrome([FromQuery] string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return BadRequest("Input string cannot be null or empty.");
+            }
+
+            var reversedString = _stringService.ReverseString(input);
+            var isPalindrome = _stringService.IsPalindrome(input);
+
+            return Ok(new
+            {
+                ReversedString = reversedString,
+                IsPalindrome = isPalindrome
+            });
+        }
+    }
+}
---
+++ Services/StringService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Application.Services
+{
+    public class StringService
+    {
+        public string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            string reversed = ReverseString(input);
+            return string.Equals(input, reversed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
